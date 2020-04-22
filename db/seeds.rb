03f2d24c69d3099a86c29f9c65b32a7b7dd7a652
markdenardo <mark.denardo@gmail.com>
2020-04-22 14:28:56 -0400

    orders seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,8 @@ end
   Sneaker.create(style:['Yeezy Boost 350','Yeezy 950','Yeezy 350 Cleat','Yeezy Boost 350 V2','Yeezy Powerphase','Yeezy Boost 700','Yeezy 500'].to_a.sample, color:['white','black','red','ochre','beige'].to_a.sample, size:rand(40...45))
 end
 
+10.times do
+  Shop.create(user_id: User.all.sample.id, sneaker_id: Sneaker.all.sample.id)
+end
+
 puts 'seeded'
